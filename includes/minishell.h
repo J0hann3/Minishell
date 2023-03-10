451b5ea5ea6a6258627af3b5fd6abedccfb29ee5
Johanne <johanne.vgn@gmail.com>
2023-03-10 16:01:39 +0100

    [ADD] struct to communicate info

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: jvigny <jvigny@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/10 15:41:48 by jvigny            #+#    #+#             */
-/*   Updated: 2023/03/10 15:44:16 by jvigny           ###   ########.fr       */
+/*   Updated: 2023/03/10 15:59:49 by jvigny           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,33 @@
 #include <sys/wait.h>
 #include <errno.h>
 #include <string.h>
+
+enum	e_meta_chararcter
+{
+	e_and,
+	e_or,
+	e_pipe
+};
+
+typedef union u_data
+{
+	struct s_instruction	*pdn_m;
+	enum					meta;
+}	t_data;
+
+typedef struct s_instruction
+{
+	char	**command;
+	char	**fd;
+}	t_instruction;
+
+typedef struct	s_node
+{
+	struct s_node	*child1;
+	struct s_node	*child2;
+	struct s_node	*parent;
+	union u_data	data;
+}	t_node;
 
 void	echo(char **arg, char **env);
 void	env(char **arg, char **env);
