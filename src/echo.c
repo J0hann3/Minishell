f032b0014bdd3223c075467df5be45740e3ea1fc
J0hann3 <johanne.vgn@gmail.com>
2023-03-11 15:26:00 +0100

    [ADD] builtin echo with option -n

--- src/echo.c
+++ src/echo.c
@@ -6,24 +6,54 @@
 /*   By: jvigny <jvigny@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/08 16:21:00 by jvigny            #+#    #+#             */
-/*   Updated: 2023/03/10 15:48:19 by jvigny           ###   ########.fr       */
+/*   Updated: 2023/03/11 15:09:10 by jvigny           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void echo(char **arg, char **env)
+static int	is_option(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (str[i] != '-')
+		return (0);
+	++i;
+	while (str[i] != '\0')
+	{
+		if (str[i] != 'n')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+void	echo(char **arg, char **env)
 {
 	int	i;
 	int	option;
+	int	check_option;
 
 	(void)env;
 	i = 1;
 	option = 1;
+	check_option = 1;
 	while (arg[i] != NULL)
 	{
+		if (i == 1 || check_option == 1)
+		{
+			if (is_option(arg[i]) == 1)
+			{
+				option = 0;
+				++i;
+				continue ;
+			}
+		}
+		check_option = 0;
 		printf("%s", arg[i]);
-		i++;
+		if (arg[++i] != NULL)
+			printf(" ");
 	}
 	if (option == 1)
 		printf("\n");
