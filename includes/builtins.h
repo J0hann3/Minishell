56e2c57305e34a1bbf5576b86c4f347e0ddedfeb
Johanne <johanne.vgn@gmail.com>
2023-03-15 19:52:25 +0100

    [ADD] .h

--- includes/builtins.h
+++ includes/builtins.h
@@ -6,7 +6,7 @@
 /*   By: jvigny <jvigny@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/10 19:41:23 by jvigny            #+#    #+#             */
-/*   Updated: 2023/03/15 14:38:13 by jvigny           ###   ########.fr       */
+/*   Updated: 2023/03/15 18:46:03 by jvigny           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,8 @@ char	*ft_strdup(const char *s);
 void	free_str(char **str);
 int		is_alpha(char c);
 int		is_digit(char c);
+int		is_variable_existing(char **env, char *str, int len_env);
+int		ft_len(char **str);
 
 // ------Init ------
 char	**init_env(const char **env);
@@ -31,5 +33,6 @@ void	echo(const char **arg, const char **env);
 void	env(char **arg, char **envp);
 char	**export(char **arg, char **env);
 int		pwd(char **arg, char **env);
+int		unset(char **arg, char **env);
 
 #endif
